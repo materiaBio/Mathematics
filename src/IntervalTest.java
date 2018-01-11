9f7d974d8c2e7a3dad14cab2ced1d5e25e3dedfe
materiaBio <gadget.tamara@gmail.com>
2018-01-11 20:43:08 +0100

    Interval compareTo method test

--- src/IntervalTest.java
+++ src/IntervalTest.java
@@ -1,10 +1,25 @@
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class IntervalTest {
 
     @Test
     public void testCompareTo() {
+        Interval a = new Interval (2,3);
+        Interval b = new Interval (2,3);
+        Interval c = new Interval (3,4);
+        Interval d = new Interval (2,4);
+        assertEquals("has to be equal", a.compareTo(b), 0);
+        assertEquals("has to be equal", b.compareTo(a), 0);
+        assertTrue ("D has to be greater then A", a.compareTo(d) < 0);
+        assertTrue ("D has to be greater then A", d.compareTo(a) > 0);
+        assertTrue ("C has to be greater then D", d.compareTo(c) < 0);
+        assertTrue ("C has to be greater then D", c.compareTo(d) > 0);
+        assertTrue ("C has to be greater then A", a.compareTo(c) < 0);
+        assertTrue ("C has to be greater then A", c.compareTo(a) > 0);
 
     }
 
